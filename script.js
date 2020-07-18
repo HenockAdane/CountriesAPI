c206358977b0bd3cdeb87a736439c1b4ed21307e
HenockAdane <65575319+HenockAdane@users.noreply.github.com>
2020-07-18 19:51:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,41 @@ document.querySelector("#theme-btn").addEventListener("click", (e) =>{
     if (document.body.style.backgroundColor === "black"){
         document.body.style.backgroundColor = "white"
         document.body.style.color = "black"
+        for(let i=0; i <document.querySelectorAll(".country").length; i++){
+            document.querySelectorAll(".country")[i].style.color = "black";
+        }
+
     }
 
     else {document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"}
+    document.body.style.color = "white"
+    for(let i=0; i <document.querySelectorAll(".country").length; i++){
+        document.querySelectorAll(".country")[i].style.color = "white";
+    }}
 })
 
 
 let search = document.querySelector("#search")
 
 let container = document.querySelector(".container");
+
+fetch(`https://restcountries.eu/rest/v2/all`).then(res => res.json()).then(data => {
+
+let output = ""
+    data.forEach((a) =>{
+        
+        output += ` <a class="country" href="#">
+        <img src = ${a.flag}>
+        <h4>${a.name}</h4>
+        <p>Population: ${a.population}</p>
+        <p>Region:${a.region}</p>
+        <p>Capital: ${a.capital}</p>
+       </a>
+`
+        container.innerHTML = output;
+    })
+})
+
 search.addEventListener("keyup", (e)=>{
 
     fetch(`https://restcountries.eu/rest/v2/name/${search.value}`).then(res => res.json()).then(data => {
@@ -35,4 +60,34 @@ let output = ""
 })
 
 
+
+let countries = document.querySelectorAll(".country");
+
+
+
+// for (let i=0; i < , i++){
+//     countries[i].addEventListener("click", (e)=>{
+//         e.preventDefault();
+//         console.log("testing")
+//     })
+// }
+
+for(let i=0; i <countries.length; i++){
+    countries[i].addEventListener("click", (e)=>{
+                e.preventDefault();
+                console.log("testing")
+            })
+}
+
+num = 0;
+document.querySelector("main").addEventListener("click", (e) =>{
+    e.preventDefault();
+    if (e.target.parentElement.classList.contains("country") || e.target.classList.contains("country")){
+        document.querySelector("main").style.display = "none";
+        document.querySelector(".more-info").style.display = "block"
+        
+        
+    }
+    
+})
  console.log(1)
